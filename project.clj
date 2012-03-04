5847d7ffd830ab60c13943947ff6bd52725f59ae
Julian Gamble <mail@juliangamble.com>
2012-03-04 14:12:30 +1100

    Final patches

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject ikeda "1.0.0"
   :description "Beauty of the mathematics of a sinkhole - captured in a Clojure GUI"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [penumbra "0.6.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :source-path "src"
 	:main ikedafunc
 )
